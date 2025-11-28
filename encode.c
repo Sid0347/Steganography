d8aeb8032ae74fce9f7ba203483cfb5a6c6cbdeb
Sid0347 <siddharthgaikwad633@gmail.com>
2025-11-28 18:27:57 +0000

    ADD do_encoding function and call other func like open_files, check_capacity, copy_bmp_header and encode_magic_str

--- encode.c
+++ encode.c
@@ -2,78 +2,55 @@
 
 /* Function Definitions */
 
-/* Get image size
- * Input: Image file ptr
- * Output: width * height * bytes per pixel (3 in our case)
- * Description: In BMP Image, width is stored in offset 18,
- * and height after that. size is 4 bytes
- */
-uint get_image_size_for_bmp(FILE *fptr_image)
-{
-    uint width, height;
-    // Seek to 18th byte
-    fseek(fptr_image, 18, SEEK_SET);
-
-    // Read the width (an int)
-    fread(&width, sizeof(int), 1, fptr_image);
-    printf("width = %u\n", width);
-
-    // Read the height (an int)
-    fread(&height, sizeof(int), 1, fptr_image);
-    printf("height = %u\n", height);
-
-    // Return image capacity
-    return width * height * 3;
-}
-
-/* 
- * Get File pointers for i/p and o/p files
- * Inputs: Src Image file, Secret file and
- * Stego Image file
- * Output: FILE pointer for above files
- * Return Value: e_success or e_failure, on file errors
- */
-Status open_files(EncodeInfo *encInfo)
-{
-    // Src Image file
-    encInfo->fptr_src_image = fopen(encInfo->src_image_fname, "r");
-    // Do Error handling
-    if (encInfo->fptr_src_image == NULL)
-    {
-    	perror("fopen");
-    	fprintf(stderr, "ERROR: Unable to open file %s\n", encInfo->src_image_fname);
-
-    	return e_failure;
-    }
-
-    // Secret file
-    encInfo->fptr_secret = fopen(encInfo->secret_fname, "r");
-    // Do Error handling
-    if (encInfo->fptr_secret == NULL)
-    {
-    	perror("fopen");
-    	fprintf(stderr, "ERROR: Unable to open file %s\n", encInfo->secret_fname);
-
-    	return e_failure;
-    }
-
-    // Stego Image file
-    encInfo->fptr_stego_image = fopen(encInfo->stego_image_fname, "w");
-    // Do Error handling
-    if (encInfo->fptr_stego_image == NULL)
-    {
-    	perror("fopen");
-    	fprintf(stderr, "ERROR: Unable to open file %s\n", encInfo->stego_image_fname);
-
-    	return e_failure;
-    }
-
-    // No failure return e_success
-    return e_success;
-}
-
 /* Perform the encoding */
 Status do_encoding(EncodeInfo *encInfo)
+{
+    /* Open files for encoding */
+    if (open_files(&encInfo) == e_failure)
+    {
+        printf("ERROR: %s function failed\n", "open_files");
+        return 1;
+    }
+    else
+    {
+        printf("SUCCESS: %s function completed\n", "open_files");
+    }
+
+    /* check capacity */
+    if (check_capacity(&encInfo) == e_failure)
+    {
+        printf("ERROR: %s function failed\n", "check_capacity");
+        return 1;
+    }
+    else
+    {
+        printf("SUCCESS: %s function completed\n", "check_capacity");
+    }
+
+    /* Copy bmp image header */
+    if (copy_bmp_header(encInfo->fptr_src_image, encInfo->fptr_stego_image) == e_failure)
+    {
+        printf("ERROR: %s function failed\n", "copy_bmp_header");
+        return 1;
+    }
+    else
+    {
+        printf("SUCCESS: %s function completed\n", "copy_bmp_header");
+    }
+
+    /* Store Magic String */
+    if (encode_magic_string(MAGIC_STRING, &encInfo) == e_failure)
+    {
+        printf("ERROR: %s function failed\n", "copy_bmp_header");
+        return 1;
+    }
+    else
+    {
+        printf("SUCCESS: %s function completed\n", "copy_bmp_header");
+    }
+}
+
+Status encode_magic_string(const char *magic_string, EncodeInfo *encInfo)
 {
     
 }
