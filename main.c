9969e6b1a572ea8f16fa2f366fcabbd2021c77af
Sid0347 <siddharthgaikwad633@gmail.com>
2025-11-28 18:32:46 +0000

    read_and_validate_enco_args and do_encoding function calling added

--- main.c
+++ main.c
@@ -1,12 +1,10 @@
-// #include <stdio.h>
 #include "encode.h"
-// #include "types.h"
 
 int main(int argc, char *argv[])
 {
     EncodeInfo encInfo;
-    uint img_size;
 
+    encInfo.argc = argc;
     // Fill with sample filenames
     // encInfo.src_image_fname = "beautiful.bmp";
     // encInfo.secret_fname = "secret.txt";
@@ -16,34 +14,31 @@ int main(int argc, char *argv[])
     int result = check_operation_type(argv);
     if (result == e_encode)
     {
+        
         // Encoding
         /* Read and validate Encode args from argv */
-        read_and_validate_encode_args(argv, &encInfo);
+        if (read_and_validate_encode_args(argv, &encInfo) == e_failure)
+        {
+            printf("The args validation failed.\n");
+            return 1;
+        }
+        if (do_encoding(&encInfo) == e_failure)
+        {
+            printf("Do encoding failed.\n");
+            return 1;
+        }
     }
     else if (result == e_decode)
     {
-        //Decode
+        // Decode
     }
     else
     {
-        //Unsupported
+        // Unsupported
     }
-
-    // Test open_files
-    if (open_files(&encInfo) == e_failure)
-    {
-    	printf("ERROR: %s function failed\n", "open_files" );
-    	return 1;
-    }
-    else
-    {
-    	printf("SUCCESS: %s function completed\n", "open_files" );
-    }
-
-    // Test get_image_size_for_bmp
-    img_size = get_image_size_for_bmp(encInfo.fptr_src_image);
-    printf("INFO: Image size = %u\n", img_size);
+   
 
     
+
     return 0;
 }
