1d6d0b9419966c6478fae347dfeeab15f5a6c510
Sid0347 <siddharthgaikwad633@gmail.com>
2025-12-11 06:13:08 +0000

    decode_byte_from_lsb function which decode a single byte from stego file

--- decode.c
+++ decode.c
@@ -24,3 +24,26 @@ Status do_decoding(DecodeInfo *decInfo)
 
     
 }
+
+/*----------------------------------------------------------------------------------*/
+/* Decode one byte from lsb.
+ * Inputs: 8 raw bytes from stego file.
+ * Output: Decoded single character.
+ * Description: Get 8 raw bytes from stego file and decode it's lsb to get encoded one charcter of secret data.
+ */
+Status decode_byte_from_lsb(unsigned char data, unsigned char *image_buffer)
+{
+    /* Check if buffer is valid or not */
+    if (image_buffer == NULL)
+    {
+        printf("ERROR: Invalid buffer data in byte_from_lsb\n");
+        return e_failure;
+    }
+    for (int i = 0; i < 8; i++)
+    {
+        int bit = image_buffer[i] & 1;
+        data = bit << (7 - i);
+    }
+    return e_success;
+}
+
