d250f0d07c38144e28ad656168e569600c8a76be
Sid0347 <siddharthgaikwad633@gmail.com>
2025-12-18 23:35:02 +0530

    Fix file opening modes and error handling in decoding functions; and open secret file, update comments for clarity

--- decode_operations.c
+++ decode_operations.c
@@ -10,7 +10,7 @@ Status read_and_validate_decode_args(char *argv[], DecodeInfo *decInfo)
     {
         decInfo->stego_image_fname = argv[2];
     }
-    else   
+    else
         return e_failure;
 
     /* Check for the output file */
@@ -27,17 +27,17 @@ Status read_and_validate_decode_args(char *argv[], DecodeInfo *decInfo)
 }
 
 /*----------------------------------------------------------------------------------*/
-/* 
- * Get File pointer for i/p file. 
+/*
+ * Open stego file in read mode.
  * Inputs: Stego Image file.
  * Output: FILE pointer for above file
  * Return Value: e_success or e_failure, on file errors
  */
 Status open_stego_file(DecodeInfo *decInfo)
 {
-    decInfo->fptr_stego_image = fopen(decInfo->stego_image_fname, "r");
+    decInfo->fptr_stego_image = fopen(decInfo->stego_image_fname, "rb");
     /* ERROR handling condition */
-    if (decInfo->stego_image_fname == NULL)
+    if (decInfo->fptr_stego_image == NULL)
     {
         perror("fopen");
         fprintf(stderr, "ERROR: Unable to open %s file\n", decInfo->stego_image_fname);
@@ -53,11 +53,32 @@ Status open_stego_file(DecodeInfo *decInfo)
  * Output: Skip header to stego image file
  * Return values: e_success or e_failure
  * Description: Skip the header part of stego file, because secret data was encoded after bmp header.
-*/
+ */
 Status skip_header(FILE *fptr_stego_fname)
 {
     if (fseek(fptr_stego_fname, 55, SEEK_END) != 0)
         return e_failure;
 
+    return e_success;
+}
+
+/*----------------------------------------------------------------------------------*/
+/*
+ *  Open secret file in write mode.
+ * Inputs: Secret file name from structure.
+ * Output: FILE pointer for above file
+ * Return Value: e_success or e_failure, on file errors
+ */
+Status open_secret_file(DecodeInfo *decInfo)
+{
+    decInfo->fptr_secret = fopen(decInfo->secret_fname, "w");
+    /* ERROR handling condition */
+    if (decInfo->fptr_secret == NULL)
+    {
+        perror("fopen");
+        fprintf(stderr, "ERROR: Unable to open %s file\n", decInfo->secret_fname);
+        return e_failure;
+    }
+    /* No error occur return success */
     return e_success;
 }
