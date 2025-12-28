ed583f44421bd65ecb8384dbc1d4227962ccc369
Sid0347 <siddharthgaikwad633@gmail.com>
2025-12-28 20:15:16 +0530

    Enhance decoding process: Add detailed success/error logging for secret file operations and fix magic string decoding

--- decode.c
+++ decode.c
@@ -35,13 +35,38 @@ Status do_decoding(DecodeInfo *decInfo)
     /* Decode secret file extenstion size */
     if (decode_secret_file_extn_size(decInfo) == e_failure)
     {
-         printf("ERROR: %s function failed\n", "decode_secret_file_extn_size");
+        printf("ERROR: %s function failed\n", "decode_secret_file_extn_size");
         return e_failure;
     }
     else
         printf("SUCCESS: %s function completed\n", "decode_secret_file_extn_size");
 
-    
+    /* Decode secret file extenstion */
+    if (decode_secret_file_extn(decInfo) == e_failure)
+    {
+        printf("ERROR: %s function failed\n", "decode_secret_file_extn");
+        return e_failure;
+    }
+    else
+        printf("SUCCESS: %s function completed\n", "decode_secret_file_extn");
+
+    /* Decode secret file size */
+    if (decode_secret_file_size(decInfo) == e_failure)
+    {
+        printf("ERROR: %s function failed\n", "decode_secret_file_size");
+        return e_failure;
+    }
+    else
+        printf("SUCCESS: %s function completed\n", "decode_secret_file_size");
+
+    /* Decode secret file data */
+    if (decode_secret_file_data(decInfo) == e_failure)
+    {
+        printf("ERROR: %s function failed\n", "decode_secret_file_data");
+        return e_failure;
+    }
+    else
+        printf("SUCCESS: %s function completed\n", "decode_secret_file_data");
 }
 
 /*----------------------------------------------------------------------------------*/
@@ -61,7 +86,7 @@ Status decode_byte_from_lsb(unsigned char *data, unsigned char *image_buffer)
     *data = 0;
     for (int i = 0; i < 8; i++)
     {
-        int bit = image_buffer[i] & 1;            
+        int bit = image_buffer[i] & 1;
         *data |= bit << (7 - i);
     }
     return e_success;
@@ -99,19 +124,25 @@ Status decode_size_from_lsb(long *size, unsigned char *image_buffer)
 Status decode_magic_string(DecodeInfo *decInfo)
 {
     char str[3];
-    printf("Enter magic string to validation : ");  /* To know wether stego file contain encoded data or not. */
-    scanf("%2[^\n]", str);
+    printf("Enter magic string to validation : "); /* To know wether stego file contain encoded data or not. */
+    scanf("%2s", str);
     unsigned char magic_string[3];
-
     for (int i = 0; i < 2; i++)
     {
         if (fread(decInfo->image_data, 1, 8, decInfo->fptr_stego_image) != 8)
+        {
+            printf("Failed to read magic string bytes\n");
             return e_failure;
-        if (decode_byte_from_lsb(decInfo->secret_data, decInfo->image_data) == e_failure)
+        }
+        if (decode_byte_from_lsb(&decInfo->secret_data, decInfo->image_data) == e_failure)
+        {
+            printf("Failed to decode magic string\n");
             return e_failure;
-        magic_string[i] = decInfo->secret_data[0];
+        }
+        magic_string[i] = decInfo->secret_data;
     }
     magic_string[2] = '\0';
+    printf("Decoded magic string : %s\n", magic_string);
     if (strcmp(str, magic_string) != 0)
     {
         printf("Magic string not matched decoding not possible\n");
@@ -150,16 +181,19 @@ Status decode_secret_file_extn(DecodeInfo *decInfo)
     {
         if (fread(decInfo->image_data, 1, 8, decInfo->fptr_stego_image) != 8)
             return e_failure;
-        
-        if (decode_byte_from_lsb(decInfo->secret_data, decInfo->image_data) == e_failure)
+
+        if (decode_byte_from_lsb(&decInfo->secret_data, decInfo->image_data) == e_failure)
             return e_failure;
 
-        decInfo->extn_secret_file[i] = decInfo->secret_data[0];
+        decInfo->extn_secret_file[i] = decInfo->secret_data;
     }
-    decInfo->extn_secret_file[4] = '\0';
+    decInfo->extn_secret_file[decInfo->size_secret_extn] = '\0';
     /* Concatenate secret file name with it's extenstion. */
+    printf("secret file name : %s\n", decInfo->secret_fname);
+
     strcat(decInfo->secret_fname, decInfo->extn_secret_file);
 
+    printf("secret file name : %s\n", decInfo->secret_fname);
     /* Open secret file in 'w' mode. */
     if (open_secret_file(decInfo) == e_failure)
         return e_failure;
@@ -179,9 +213,29 @@ Status decode_secret_file_size(DecodeInfo *decInfo)
 
     if (fread(buffer, 1, 32, decInfo->fptr_stego_image) != 32)
         return e_failure;
-    
-    if(decode_size_from_lsb(&decInfo->size_secret_file, buffer) == e_failure)
+
+    if (decode_size_from_lsb(&decInfo->size_secret_file, buffer) == e_failure)
         return e_failure;
-    
+
     return e_success;
+}
+
+/*----------------------------------------------------------------------------------*/
+/* Decode secret file data.
+ * Inputs: Encoded bytes.
+ * Output: Decoded data.
+ * Description: Get no of secret file size encoded bytes from stego file and write it in secret file by extracting it.
+ */
+Status decode_secret_file_data(DecodeInfo *decInfo)
+{
+    for (int i = 0; i < decInfo->size_secret_file; i++)
+    {
+        if (fread(decInfo->image_data, 1, 8, decInfo->fptr_stego_image) != 8)
+            return e_failure;
+
+        if (decode_byte_from_lsb(&decInfo->secret_data, decInfo->image_data) == e_failure)
+            return e_failure;
+        /* fputc() because we are writing only one character in secret file. */
+        fputc(decInfo->secret_data, decInfo->fptr_secret);
+    }
 }
